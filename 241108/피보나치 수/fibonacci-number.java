f6fc8dfe9528c000fa3cc3e70af15d589214e972
mailofk <104469091+mailofk@users.noreply.github.com>
2024-11-08 13:50:37 +0900

    Title: 피보나치 수, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 241108/피보나치 수/fibonacci-number.java
+++ 241108/피보나치 수/fibonacci-number.java
@@ -9,9 +9,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new int[n+1];
+        dp = new int[46];
 
-        dp[1]=1; dp[2]=1;
+        dp[1]=1; 
+        dp[2]=1;
         for (int i=3;i<=n;i++)
             dp[i] = dp[i-1] + dp[i-2];
 
