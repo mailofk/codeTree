64138ebab138560696cf2becefe972e21d28743f
mailofk <104469091+mailofk@users.noreply.github.com>
2024-11-08 18:59:40 +0900

    Title: 사각형 채우기 3, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 241108/사각형 채우기 3/rectangle-fill-3.java
+++ 241108/사각형 채우기 3/rectangle-fill-3.java
@@ -4,19 +4,19 @@ import java.io.*;
 public class Main {
 
     public static int n;
-    public static int[] dp;
+    public static long[] dp;
     public static final int MOD = 1000000007;
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new int[1001];
+        dp = new long[1001];
         dp[0]=1; dp[1]=2; dp[2] = 7;
 
-        for (int i=3;i<=n;i++) {
+        for (int i=2;i<=n;i++) {
             dp[i] = (dp[i-1]*2 + dp[i-2]*3)%MOD;
             for (int j = i-3;j>=0;j--) {
-                dp[i] = (dp[i] + (dp[j]*2)%MOD)%MOD;
+                dp[i] = (dp[i] + dp[j]*2)%MOD;
             }
         }
 
